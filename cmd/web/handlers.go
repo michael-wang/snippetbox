18657fc6bbb51172cd68156bdfea6a0ebc4037b8
Michael <michael.icwang@gmail.com>
2020-09-28 00:22:21 +0800

    Implement db Insert method and use it in handler.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,5 +46,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	// Dummy data.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
