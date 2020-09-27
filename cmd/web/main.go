f82a3ba6fbaf94b9e73b7dbf163e3118d94f91d9
Michael <michael.icwang@gmail.com>
2020-09-27 12:39:14 +0800

    Using customized info/error logger.
    Inject dependency to handler function through package level struct (application.)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,14 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+// application struct holds application-wide dependencies.
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 	// addr contains command-line flag 'addr'.
@@ -13,10 +20,20 @@ func main() {
 	// Must call Parse after setup all flags, and before using them.
 	flag.Parse()
 
+	// With custom logs redirect to standard outputs we can:
+	// `go run ./cmd/web >>/tmp/info.log 2>>/tmp/error.log`
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	// Create a file server to serve files out of "./ui/static" directory.
 	// Note: the path to http.Dir is relative to project root.
@@ -27,7 +44,13 @@ func main() {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
