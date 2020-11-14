48cff4c9c3bc3fdd6ed6894c1ac522ff2029700b
Michael <michael.icwang@gmail.com>
2020-11-14 13:16:47 +0800

    Add common data to all templates.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // Common code to handle server internal error:
@@ -28,6 +29,14 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	ts, ok := app.templateCache[name]
 	if !ok {
@@ -39,7 +48,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	// Write template to a buffer instead of straight to http.ResponseWriter.
 	// If any thing goes wrong, response client with the error.
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
