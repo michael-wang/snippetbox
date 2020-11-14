5228fe9ea51b01c769a69df933977a09a9d21503
Michael <michael.icwang@gmail.com>
2020-11-14 13:33:35 +0800

    Define, register, and use custom template function.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/michael-wang/snippetbox/pkg/models"
 )
@@ -11,6 +12,17 @@ type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+// humanDate turns t into human friendly string.
+// It's supposed to be used as custom template function,
+// which can only has one reutrn value (or 2 as 2nd be error.)
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -24,7 +36,10 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		// We have to register custom template function before ParseFiles.
+		// So we have to use template.New to create empty template set and
+		// register custom template function with Funcs() method.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
