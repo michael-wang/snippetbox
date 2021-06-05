457edde9c3afce8b9bea5067d2627dee6d2ebb2a
Michael <michael.icwang@gmail.com>
2021-06-05 22:56:44 +0800

    run server on https

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -51,6 +51,7 @@ func main() {
 
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = 12 * time.Hour
+	session.Secure = true
 
 	app := application{
 		errorLog:      errorLog,
@@ -67,7 +68,7 @@ func main() {
 		Handler:  app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
