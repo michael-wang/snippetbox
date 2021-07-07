0ac4ed58376af00096366043ba5d743c3d5ba90e
Michael <michael.icwang@gmail.com>
2021-07-07 23:36:48 +0800

    Implement authorization

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -35,6 +35,8 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 	}
 	td.CurrentYear = time.Now().Year()
 	td.Flash = app.session.PopString(r, "flash")
+
+	td.IsAuthenticated = app.isAuthenticated(r)
 	return td
 }
 
@@ -58,4 +60,8 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// Now we know there is no error rendering the template,
 	// so write the rendered buffer to http.ResponseWriter.
 	buf.WriteTo(w)
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.session.Exists(r, "authenticatedUserID")
 }
