e19067753c082683c4c4b883f4c4fa11570dcb14
Michael <michael.icwang@gmail.com>
2021-07-10 00:45:55 +0800

    add pattern for testing db

---
+++ pkg/models/mysql/users_test.go
@@ -0,0 +1,65 @@
+package mysql
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/michael-wang/snippetbox/pkg/models"
+)
+
+func TestUserModelGet(t *testing.T) {
+	if testing.Short() {
+		t.Skip("mysql: skipping integration test")
+	}
+
+	tests := []struct {
+		name      string
+		userID    int
+		wantUser  *models.User
+		wantError error
+	}{
+		{
+			name:   "Valid ID",
+			userID: 1,
+			wantUser: &models.User{
+				ID:      1,
+				Name:    "Alice Jones",
+				Email:   "alice@example.com",
+				Created: time.Date(2018, 12, 23, 17, 25, 22, 0, time.UTC),
+				Active:  true,
+			},
+			wantError: nil,
+		},
+		{
+			name:      "Zero ID",
+			userID:    0,
+			wantUser:  nil,
+			wantError: models.ErrNoRecord,
+		},
+		{
+			name:      "Non-existent ID",
+			userID:    2,
+			wantUser:  nil,
+			wantError: models.ErrNoRecord,
+		},
+	}
+
+	for _, c := range tests {
+		t.Run(c.name, func(t *testing.T) {
+			db, teardown := newTestDB(t)
+			defer teardown()
+
+			m := UserModel{db}
+
+			user, err := m.Get(c.userID)
+			if err != c.wantError {
+				t.Errorf("error want %v; but got: %s", c.wantError, err)
+			}
+
+			if !reflect.DeepEqual(user, c.wantUser) {
+				t.Errorf("user want: %v, got: %v", c.wantUser, user)
+			}
+		})
+	}
+}
